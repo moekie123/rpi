30d16999b8c1ce71b29bd62e0e3622eb9fa98720
Ron Salm <rsalm@oceaneering.com>
2021-10-13 11:06:21 +0000

    fix: Update linker after installation of toolkit

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 docker rm devel
-docker build . -t environment:1.1.0 -t environment:latest
+docker build . -t environment:1.1.1 -t environment:latest
 docker run -dit -P --name devel -v ~/project:/root/project environment:latest /bin/bash
 
