487e5cd9d0114eafdd50fc986066162f1459c191
Ron Salm <rsalm@oceaneering.com>
2021-11-11 19:00:57 +0000

    feat: Added an info page for a fresh build
    Doxygen documentation needs to be generated before it can be load.
    An instruction page is added to the project that is load at http

---
+++ docs/html/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+	<div class="center">
+	<table>
+		<tbody>
+			<tr class="h">
+				<td>
+					Login to the development environment 'dca' and run 'build doc' to generated doxygen
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	</div>
+
+	<?php phpinfo( ); ?>
+
+    </body>
+</html>
+
+
